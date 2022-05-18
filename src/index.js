c6a5acf6091f3fbb22c732e8db2b3f19edd1ee6c
Chris Hannaby <chris.hannaby@netlify.com>
2022-05-18 14:14:27 -0600

    await stuff

--- src/index.js
+++ src/index.js
@@ -60,8 +60,8 @@ export const onBuild = async function ({
   const BUILD_ID = process.env.INCOMING_HOOK_BODY
   const key = `${BUILD_ID}.tgz`
   await downloadFromS3(key)
-  run.command('ls -la')
-  run.command(`mkdir ${PUBLISH_DIR}`)
-  run.command(`tar --strip-components 1 -vxzf ${key} -C ${PUBLISH_DIR}`)
+  await run.command('ls -la')
+  await run.command(`mkdir ${PUBLISH_DIR}`)
+  await run.command(`tar --strip-components 1 -vxzf ${key} -C ${PUBLISH_DIR}`)
   await unlink(key)
 }
